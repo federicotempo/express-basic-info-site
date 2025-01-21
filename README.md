d64f1200f57af8aa2ca1269d0b85d4b51aaa198a
federicotempo <federicotempo98@gmail.com>
2025-01-21 09:30:31 -0300

    Refactor project with Express
    -refactor app.js

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# node-basic-info-site
- This project is a basic informational website built with Node.js as part of a practical exercise to apply foundational knowledge. The site includes the following pages:
+# Express-basic-info-site
+ This project is a basic informational website built with Express as part of a practical exercise to apply foundational knowledge. The site includes the following pages:
 
 1. Index: A homepage with a brief introduction.
 2. About: A general information page.
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
